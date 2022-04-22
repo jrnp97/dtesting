3d6db6c717faa0fb1a01f5a1ea7e99af5f0c0ee0
jrnp97 <jrnp1997@gmail.com>
2022-04-22 11:35:20 -0300

    feat: Define our custom login

--- dtesting/accounts/views.py
+++ dtesting/accounts/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/my_login.html'
+
 
 @login_required
 def test_celery(request):
